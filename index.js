5e9d52227da80b860d9149d9364a354bd3ae72a2
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-30 11:00:18 +0600

    added POST method to create a new user

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 // dbuser2:XB2rFYL2u00IJOsU
 const uri = "mongodb+srv://dbuser2:XB2rFYL2u00IJOsU@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        const usersCollection = client.db('nodeMongoCrud').collection('users');
+
+        // create data (POST method)
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        });
+
+    } finally { }
+}
+
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
     res.send('CRUD Server is running');
