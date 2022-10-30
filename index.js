0f0ab20776f550f71b857f617545376addde4670
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-30 08:36:24 +0600

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// dbuser2:XB2rFYL2u00IJOsU
+const uri = "mongodb+srv://dbuser2:XB2rFYL2u00IJOsU@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send('CRUD Server is running');
