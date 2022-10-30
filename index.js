c34dd5e7678a5b5db8ea954303d6e91e2db65e24
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-30 08:35:07 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('CRUD Server is running');
+})
+
+app.listen(port, () => {
+    console.log('server is running on', port);
+})
